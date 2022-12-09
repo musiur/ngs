3b4e30bac9c40fc42a7a5b62ae9a649c54b49d30
Musiur <musiur.opu@gmail.com>
2022-12-09 14:04:12 +0600

    teacher page modified to teacher login page

--- pages/teachers.js
+++ pages/teachers.js
@@ -1,11 +1,11 @@
-import AllTeachers from "../components/admin/teacher/AllTeachers";
+
 import Layout from "../Layout/Layout";
 const Teachers = () => {
   return (
     <Layout>
       <div className="containerG sectionG">
-      <h2 className="text-xl lg:text-2xl font-bold pb-10">All Teachers</h2>
-        <AllTeachers />
+      <h2 className="text-xl lg:text-2xl font-bold pb-10">Login</h2>
+        <div>Login component</div>
       </div>
     </Layout>
   );
